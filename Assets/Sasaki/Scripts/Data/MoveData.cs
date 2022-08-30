f7e4b6558e2c22b5b68dff3b384ef374f98fadf3
SasakiHayato0605 <zuomusundou685@gmail.com>
2022-08-30 15:58:05 +0900

    Enemyの行動データの作成、完了

--- Assets/Sasaki/Scripts/Data/MoveData.cs
+++ Assets/Sasaki/Scripts/Data/MoveData.cs
@@ -1,18 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
+public enum MoveType
+{
+    Forward,
+    ToPlayer,
+}
+
+/// <summary>
+/// Enemyの行動データclass
+/// </summary>
 
 [System.Serializable]
 public class MoveData
 {
+    [SerializeField] MoveType _moveType;
     [SerializeField] float _speed;
     [SerializeField] ShakeCondition _shakeCondition;
 
+    /// <summary>
+    /// 揺れ幅のデータクラス
+    /// </summary>
     [System.Serializable]
     class ShakeCondition
     {
-        [SerializeField, Range(-1, 1)] float _horizontal = 0;
-        [SerializeField, Range(-1, 1)] float _verticle = 0;
+        [SerializeField, Range(0, 1)] float _horizontal = 0;
+        [SerializeField, Range(0, 1)] float _verticle = 0;
 
         [SerializeField] float _size = 1;
         [SerializeField] float _loopSpeed = 1;
@@ -23,8 +35,19 @@ public class MoveData
         public float LoopSpeed => _loopSpeed;
     }
 
+    /// <summary>
+    /// 行動タイプ
+    /// </summary>
+    public MoveType MoveType => _moveType;
+
+    /// <summary>
+    /// Enemyのスピード
+    /// </summary>
     public float Speed => _speed;
 
+    /// <summary>
+    /// 振れ具合
+    /// </summary>
     public Vector2 Shake
     {
         get
@@ -38,5 +61,13 @@ public class MoveData
         }
     }
 
+    /// <summary>
+    /// 振れ具合の大きさ
+    /// </summary>
+    public float ShakeSize => _shakeCondition.Size;
+
+    /// <summary>
+    /// 振れ具合の速さ
+    /// </summary>
     public float LoopSpeed => _shakeCondition.LoopSpeed;
 }
--- Assets/Sasaki/Scripts/Data/MoveOperator.cs
+++ Assets/Sasaki/Scripts/Data/MoveOperator.cs
@@ -1,15 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 行動データの補正
+/// </summary>
 public class MoveOperator : MonoBehaviour
 {
     [SerializeField] MoveData _moveData;
 
-    float _timer;
+    float _shakeTimer;
     bool _isRequest;
-
-
 
     void Update()
     {
@@ -18,26 +17,60 @@ public class MoveOperator : MonoBehaviour
             return;
         }
 
-        _timer += Time.deltaTime;
-
-
+        _shakeTimer += Time.deltaTime * _moveData.LoopSpeed;
     }
 
+    /// <summary>
+    /// 補正されたデータを返す
+    /// </summary>
+    /// <param name="t">使用者</param>
+    /// <returns>Velocity</returns>
     public Vector3 MoveCollect(Transform t)
     {
-        Vector3 forward = t.forward;
-
-
-
-        return Vector3.zero;
+        Vector3 forward = Rotate(t) * _moveData.Speed;
+        Vector2 dir = SetDir() * _moveData.ShakeSize;
+        
+        return new Vector3(forward.x + dir.x, forward.y + dir.y, forward.z);
     }
 
+    Vector3 Rotate(Transform t)
+    {
+        Vector3 dir;
+
+        if (_moveData.MoveType == MoveType.ToPlayer)
+        {
+            // tbd Playerを想定
+            dir = t.forward;
+        }
+        else
+        {
+            dir = t.forward;
+        }
+
+        return dir;
+    }
+
+    Vector2 SetDir()
+    {
+        float sin = Mathf.Sin(_shakeTimer) * _moveData.Shake.x;
+        float cos = Mathf.Cos(_shakeTimer) * _moveData.Shake.y;
+
+        return new Vector2(sin, cos);
+    }
+
+    /// <summary>
+    /// 初期化
+    /// </summary>
     public void Initalize()
     {
-        _timer = 0;
+        _shakeTimer = 0;
         _isRequest = false;
     }
 
+    /// <summary>
+    /// 行動データの補正リクエスト。Trueの場合補正を行う
+    /// </summary>
+    /// <param name="isRequest">Request</param>
     public void OprationRequest(bool isRequest)
     {
         _isRequest = isRequest;
